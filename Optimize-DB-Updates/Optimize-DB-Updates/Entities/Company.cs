6a85bd8fbf8d3e05b9abede7824691a067e377ee
Dylan <dylan_krajnc@tutanota.com>
2023-12-14 11:34:51 +0000

    - Endpoints
    - added three minimal API endpoints that update data in an example database using EF-Core, SQL and Dapper - with comparisons of speed added as comments for each endpoint

---
+++ Optimize-DB-Updates/Optimize-DB-Updates/Entities/Company.cs
@@ -0,0 +1,10 @@
+﻿namespace Optimize_DB_Updates.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime? LastSalaryUpdateUtc { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
